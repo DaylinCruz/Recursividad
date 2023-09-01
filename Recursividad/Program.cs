94090f23fd8391190b737a04bb49cbcd60ae6e6c
dayli <dayli@DESKTOP-VP1QNAQ>
2023-08-31 21:33:47 -0600

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Escribe un número para encontrar su suma y factorial mediante recursividad");
+int i = int.Parse(Console.ReadLine());
+
+Console.Write("\nSUMA: ");
+Console.Write(suma(i));
+
+Console.Write("\nFACTORIAL: ");
+Console.WriteLine(factorial(i));
+
+
+static int suma (int n)
+{
+    if (n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n + suma(n - 1);
+    }
+}
+
+static int factorial (int n)
+{
+    if (n==1)
+    {
+        return n;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+
+ 
